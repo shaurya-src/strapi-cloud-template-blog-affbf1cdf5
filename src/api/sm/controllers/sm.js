7f44d0fe1714ed6d5787ec971739b1e2caae48e1
Shaurya Choudhary <shaurya.src@gmail.com>
2025-05-02 23:25:15 +0530

    update collection

--- src/api/sm/controllers/sm.js
+++ src/api/sm/controllers/sm.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * sm controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::sm.sm');
--- src/api/sm/routes/sm.js
+++ src/api/sm/routes/sm.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * sm router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::sm.sm');
